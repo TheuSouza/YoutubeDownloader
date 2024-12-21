e97ff98dd676c19f3e67dcc4537ad7706fb9a3d5
TheuSouza <iam.mths.s@gmail.com>
2024-12-20 21:49:24 -0300

    Código Python para Download de Vídeo do Youtube

--- YoutubeDownload.py
+++ YoutubeDownload.py
@@ -1,12 +1,15 @@
 import yt_dlp
 
+# solicita o link do vídeo.
 link = input('Link do vídeo para Download: ').strip()
 
+# Cria um dict com o melhor formato disponivel do video.
 ydl_opts = {
     'format': 'bestvideo+bestaudio/best',
     'outtmpl': 'video_download.%(ext)s',
 }
 
+# Faz um Try para baixar o video, caso ocorra algum erro o mesmo é printado na tela.
 try:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([link])
